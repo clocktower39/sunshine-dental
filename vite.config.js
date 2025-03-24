0931f133d252f33b11afdcb3a38b2d322494ecb3
clocktower39 <clocktower39@aol.com>
2025-03-23 18:33:36 -0700

    configure rollup, upgrade packages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          "react-vendor": ["react", "react-dom", "react-router", "react-router-hash-link"],
+          "mui-vendor": [
+            "@mui/material",
+            "@mui/icons-material",
+            "@emotion/react",
+            "@emotion/styled",
+          ],
+          fonts: ["@fontsource/poppins", "@fontsource/roboto"],
+        },
+      },
+    },
+  },
+});
