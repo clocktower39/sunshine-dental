c071bda4c92c91035c5571bd8e2f49004e99fdf9
clocktower39 <clocktower39@aol.com>
2025-06-16 21:18:58 -0700

    add Navbar and footer to 404 Page

--- src/Pages/NotFoundPage.jsx
+++ src/Pages/NotFoundPage.jsx
@@ -1,20 +1,26 @@
 import React from "react";
 import { Button, Grid, Typography } from "@mui/material";
 import { Link } from "react-router";
+import WebsiteNavbar from "../Components/WebsiteNavbar";
+import Footer from "../Components/Footer";
 
 export default function NotFoundPage() {
   return (
-    <Grid container sx={{ alignContent: 'center', height: 'calc(100vh - 20px)'}} > {/* make this expand to fill the entire parent container height and center the elements horizontally and vertically */}
-      <Grid container size={12} sx={{ alignItems: "center", justifyContent: "center" }}>
-        <Typography textAlign="center" variant="h4">
-          404: Page Not Found
-        </Typography>
+    <>
+      <WebsiteNavbar />
+      <Grid container sx={{ alignContent: "center", height: "calc(100vh - 200px)" }}>
+        <Grid container size={12} sx={{ alignItems: "center", justifyContent: "center" }}>
+          <Typography textAlign="center" variant="h4">
+            404: Page Not Found
+          </Typography>
+        </Grid>
+        <Grid container size={12} sx={{ alignItems: "center", justifyContent: "center" }}>
+          <Button component={Link} to="/">
+            Go Home
+          </Button>
+        </Grid>
       </Grid>
-      <Grid container size={12} sx={{ alignItems: "center", justifyContent: "center" }}>
-        <Button component={Link} to="/">
-          Go Home
-        </Button>
-      </Grid>
-    </Grid>
+      <Footer />
+    </>
   );
 }
